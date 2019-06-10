343ead095f3d72b6424a0e47b1c09ce66526aab2
alinlastun <alinlastun@gmail.com>
2019-06-10 09:35:46 +0300

    shown sortDialog when click sort settings

--- app/src/main/java/com/example/githubtraining/screen/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/githubtraining/screen/settings/SettingsActivity.kt
@@ -1,26 +1,41 @@
 package com.example.githubtraining.screen.settings
 
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import com.example.githubtraining.R
-import kotlinx.android.synthetic.main.activity_settings.*
 import android.arch.lifecycle.ViewModelProviders
-import com.example.githubtraining.screen.login.LoginViewModel
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import com.example.githubtraining.R
 import com.example.githubtraining.utill.LocalViewModelFactory
-import kotlinx.android.synthetic.main.content_settings.*
 
 
 class SettingsActivity : AppCompatActivity() {
 
     private lateinit var mViewModel: SettingsViewModel
-
+    private lateinit var mBinding: com.example.githubtraining.databinding.ActivitySettingsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
-        setSupportActionBar(toolbar)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_settings)
+        mBinding.activity = this
+        mViewModel = ViewModelProviders.of(this, LocalViewModelFactory(this)).get(SettingsViewModel::class.java)
 
-        mViewModel =ViewModelProviders.of(this, LocalViewModelFactory(this)).get(SettingsViewModel::class.java)
+    }
 
+    fun onClickSortRepo() {
+        showSortDialog()
+    }
+
+    private fun showSortDialog() {
+        val dialogBuilder = AlertDialog.Builder(this)
+        val inflater = this.layoutInflater
+        val dialogView = inflater.inflate(R.layout.dialog_sort, null)
+        dialogBuilder.setView(dialogView)
+        dialogBuilder.setCancelable(false)
+        val alertDialog = dialogBuilder.create()
+        val btnCancel = dialogView.findViewById<TextView>(R.id.nCancelDialog)
+        btnCancel?.setOnClickListener { alertDialog.dismiss() }
+        alertDialog.show()
     }
 
 }
